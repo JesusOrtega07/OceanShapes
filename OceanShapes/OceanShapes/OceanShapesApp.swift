b6b87f165abcd165121085f3f771af678f739e0e
JesusOrtega07 <jesus.ortega07@unach.mx>
2025-06-20 11:14:32 -0600

    First Commit

---
+++ OceanShapes/OceanShapes/OceanShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OceanShapesApp.swift
+//  OceanShapes
+//
+//  Created by Jesus Ortega on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct OceanShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
